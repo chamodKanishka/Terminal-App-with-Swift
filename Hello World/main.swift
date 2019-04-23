ffa6a3e2dd29676f3b18defad2cdb89cf845339b
ChamodKanishka <chamodkanishka77@gmail.com>
2019-04-23 16:07:29 +0530

    first commit

---
+++ Hello World/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Hello World
+//
+//  Created by chamod kanishka chathuranga on 4/23/19.
+//  Copyright © 2019 chamod kanishka chathuranga. All rights reserved.
+//
+
+import Foundation
+
+func hello(name: String, surname: String) {
+    print("Hello My Name is \(name) \(surname)")
+}
+let myName = "Chamod"
+let mySecondname = "Kanishka"
+hello(name: myName, surname: mySecondname)
